77ef2c5f2878fdcd498c67ad7d6c46ee162f0772
loanalix <117281608+loanalix@users.noreply.github.com>
2022-12-14 15:27:40 +0100

    fini vrai

--- src/components/Pokemonlist.js
+++ src/components/Pokemonlist.js
@@ -4,7 +4,7 @@ import { getAll } from "../api/pokemon";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Stack from 'react-bootstrap/Stack'
+
 
 
 function Pokemonlist(props){
